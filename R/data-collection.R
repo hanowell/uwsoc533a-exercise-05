ce6d372b6fb67a8ebfece412f59ef458976a8131
Ben Hanowell <ben@hanowell.me>
2022-02-10 20:47:51 -0800

    Initialize assignment

---
+++ R/data-collection.R
@@ -0,0 +1,30 @@
+# Data collection for exercise 5 ----
+
+# Libraries ----
+library(dplyr)
+library(readr)
+
+# Age-specific death rates for Russia in 1997 ----
+rus_mxi <- readr::read_csv("raw/RUS_m_short_idr.csv") %>%
+  dplyr::filter(sex == 3, year == 1997) %>%
+  dplyr::mutate(disease_type = dplyr::case_when(
+    cause == 0 ~ "all",
+    cause == 7 ~ "heart",
+    cause %in% c(1:6, 8:16) ~ "other"
+  )) %>%
+  dplyr::group_by(disease_type) %>%
+  dplyr::summarize_at(vars(m0:m85p), ~sum(.)/1e6) %>%
+  dplyr::ungroup() %>%
+  tidyr::pivot_longer(
+    cols = m0:m85p,
+    names_prefix = "m",
+    names_to = "x",
+    values_to = "mx"
+  ) %>%
+  tidyr::pivot_wider(
+    id_cols = x,
+    names_from = disease_type,
+    values_from = mx,
+    names_prefix = "mx_"
+  )
+saveRDS(rus_mxi, "data/rus_mxi.rds")
